0b82965ed7360ea4c47ac564650dbed764f5b67c
Koios1143 <ken1357924681010@gmail.com>
2023-06-16 11:29:49 +0800

    Rename function and return reasonable result in GetRouteArrival

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 import json
+import copy
 from datetime import datetime
 
 app_id = 'koios1143-d2c91cc5-2c58-4a06'
@@ -48,41 +49,49 @@ def SendRequest(request_url):
     data_response = requests.get(request_url, headers=d.get_data_header())
     return json.loads(data_response.text)
 
-def GetFastestBus(route_name):
+def GetRouteDetail(route_name):
     city = 'Hsinchu'
-    args = '?format=JSON&orderby="EstimateTime"'
-    api_type = 'EstimatedTimeOfArrival/Streaming/City/{}/{}{}'.format(city, route_name, args)
+    args = '?format=JSON'
+    api_type = 'StopOfRoute/City/{}/{}{}'.format(city, route_name, args)
     request_url = base_url.format(api_type)
 
     result = SendRequest(request_url)
-    # index = 0
-    # while(result[index]['PlateNumb'] == '-1'):
-    #     index += 1
-    # index += 1
 
+    with open('routes_detail.json', 'w') as f:
+        f.write(json.dumps(result))
+
+def GetRouteArrival(route_name, direction):
+    city = 'Hsinchu'
+    args = '?format=JSON'
+    api_type = 'EstimatedTimeOfArrival/Streaming/City/{}/{}{}'.format(city, route_name, args)
+    request_url = base_url.format(api_type)
+
+    request_result = SendRequest(request_url)
     current_time = datetime.now()
-    for index in range(len(result)):
-        #if(result[index]['PlateNumb'] == '-1'):
-        #    continue
-        try:
-            result[index]['EstimateTime']
-        except:
-            continue
-        src_trans_time = datetime.fromisoformat(result[index]['SrcTransTime']).replace(tzinfo=None)
-        #print(current_time, src_trans_time)
+    return_result = json.loads("[]")
+    element_template = json.loads('{"name": "", "arrivalTime": -1}')
 
-        with open('output.json', 'w') as f:
-            f.write(json.dumps(result))
-        
+    for index in range(len(request_result)):
+        if(request_result[index]['PlateNumb'] == '-1' or
+           request_result[index]['Direction'] != direction or
+           'EstimateTime' not in request_result[index]):
+           continue
+
+        src_trans_time = datetime.fromisoformat(request_result[index]['SrcTransTime']).replace(tzinfo=None)
         trans_time_delta = int((current_time - src_trans_time).total_seconds())
-        estimate_time = int((int(result[index]['EstimateTime']) - trans_time_delta) / 60)
-        #print(trans_time_delta, int(result[index]['EstimateTime']))
+        estimate_time = int((int(request_result[index]['EstimateTime']) - trans_time_delta) / 60)
 
-        print('[{}]\t\t{:2d}mins\t{}'.format(result[index]['StopName']['Zh_tw'], estimate_time, result[index]['PlateNumb']))
-    #return estimate_time
+        template = copy.deepcopy(element_template)
+        template['name'] = request_result[index]['StopName']['Zh_tw']
+        template['arrivalTime'] = estimate_time
+        return_result.append(template)
+
+    return return_result
 
 
 
 if __name__ == '__main__':
     route_name = '182'
-    GetFastestBus(route_name)
+    result = GetRouteArrival(route_name, 0)
+    print(json.dumps(result, ensure_ascii=False))
+    #GetRouteDetail(route_name)
