35648fdafdc271174819786d65d445d12e729d8a
Koios1143 <ken1357924681010@gmail.com>
2023-06-17 14:53:52 +0800

    Remove redundant code again

--- StopsDetailFiles/temp.py
+++
@@ -1,8 +0,0 @@
-import json
-
-f = json.loads(open('CityBus.json', 'r').read())
-print(len(f))
-f = json.loads(open('CountyBus.json', 'r').read())
-print(len(f))
-f = json.loads(open('InterCityBus.json', 'r').read())
-print(len(f))
